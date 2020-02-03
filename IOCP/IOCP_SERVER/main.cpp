8f8e682ee8734ef5b9cb00f93aa6dde6150104f8
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-03 14:51:04 +0900

    기본적인 Thread 생성,사용

--- IOCP/IOCP_SERVER/main.cpp
+++ IOCP/IOCP_SERVER/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <Windows.h>
+#include <process.h>
 
-int main()
+unsigned WINAPI ThreadFunc(void* arg);
+
+int main(int argc, char* argv[])
 {
-	std::cout << "aa" << std::endl;
+	HANDLE hThread;
+	unsigned threadID;
+	int param = 5;
+
+	hThread = (HANDLE)_beginthreadex(NULL, 0, ThreadFunc, (void*)&param, 0, &threadID);
+	if (hThread == 0)
+	{
+		std::cout << "_beginthreadex() error" << std::endl;
+		return -1;
+	}
+
+	Sleep(3000);
+	std::cout << "end Main()" << std::endl;
+
+	return 0;
+}
+
+unsigned WINAPI ThreadFunc(void* arg)
+{
+	int i = 0;
+	int cnt = *((int*)arg);
+	for (int i = 0; i < cnt; i++)
+	{
+		Sleep(1000);
+		std::cout << "running Thread" << std::endl;
+	}
+	return 0;
 }
