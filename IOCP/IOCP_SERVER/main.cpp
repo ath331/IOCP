1f5ce65dbc80f94d0667672406cad417a791dba3
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-06 17:05:39 +0900

    CompRoutine을 이용한 중첩IO

--- IOCP/IOCP_SERVER/main.cpp
+++ IOCP/IOCP_SERVER/main.cpp
@@ -3,24 +3,23 @@
 #include <WinSock2.h>
 
 const static int BUF_SIZE = 1024;
+void CALLBACK CompRoutine(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
 void ErrorHandling(const char* msg);
+
+WSABUF dataBuf;
+char buf[BUF_SIZE];
+int recvBytes = 0;
 
 int main(int argc, const char* argv[])
 {
 	WSADATA wsaData;
 	SOCKET hLisnSock, hRecvSock;
-	SOCKADDR_IN lisnAdr,recvAdr;
-
-	WSABUF dataBuf;
-	memset(&dataBuf, 0, sizeof(dataBuf));
-	int recvAdrSz = 0;
+	SOCKADDR_IN lisnAdr, recvAdr;
 
 	WSAEVENT evObj;
 	WSAOVERLAPPED overlapped;
 
-	char buf[BUF_SIZE];
-	int recvBytes = 0, flags = 0;
-
+	int idx = 0, recvAdrSz = 0, flags = 0;
 	if (argc != 2)
 	{
 		printf("Usage : %s <port>\n", argv[0]);
@@ -45,33 +44,45 @@ int main(int argc, const char* argv[])
 
 	recvAdrSz = sizeof(recvAdr);
 	hRecvSock = accept(hLisnSock, (SOCKADDR*)&recvAdr, &recvAdrSz);
+	if (hRecvSock == INVALID_SOCKET)
+		ErrorHandling("accept() error");
 
-	evObj = WSACreateEvent();
+	evObj = WSACreateEvent(); //dummy
 	memset(&overlapped, 0, sizeof(overlapped));
 	overlapped.hEvent = evObj;
 	dataBuf.len = BUF_SIZE;
 	dataBuf.buf = buf;
 
-	if (WSARecv(hRecvSock, &dataBuf, 1, (LPDWORD)&recvAdrSz, (LPDWORD)&flags, &overlapped, NULL) == SOCKET_ERROR)
+	if (WSARecv(hRecvSock, &dataBuf, 1, (LPDWORD)&recvAdrSz, (LPDWORD)&flags, &overlapped, CompRoutine) == SOCKET_ERROR)
 	{
 		if (WSAGetLastError() == WSA_IO_PENDING)
 		{
 			puts("BackGround data recv");
-			WSAWaitForMultipleEvents(1, &evObj, TRUE, WSA_INFINITE, FALSE);
-			WSAGetOverlappedResult(hRecvSock, &overlapped, (LPDWORD)&recvBytes, FALSE, NULL);
-		}
-		else
-		{
-			ErrorHandling("WSARecv() error");
 		}
 	}
 
-	printf("Recv data : %s \n", buf);
+	idx = WSAWaitForMultipleEvents(1, &evObj, FALSE, WSA_INFINITE, TRUE);
+	if (idx = WAIT_IO_COMPLETION)
+		puts("\nOverlapped I/O Completed!");
+	else
+		ErrorHandling("WSARecv() error");
+
 	WSACloseEvent(evObj);
 	closesocket(hLisnSock);
 	closesocket(hRecvSock);
 	WSACleanup();
 	return 0;
+}
+
+void CALLBACK CompRoutine(DWORD dwError, DWORD szRecvBytes, LPWSAOVERLAPPED lpOverlapped, DWORD falgs)
+{
+	if (dwError != 0)
+		ErrorHandling("CompRoutine error!");
+	else
+	{
+		recvBytes = szRecvBytes;
+		printf("Recv Data : %s ", buf);
+	}
 }
 
 void ErrorHandling(const char* msg)
