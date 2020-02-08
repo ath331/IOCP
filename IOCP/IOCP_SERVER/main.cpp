f913e330d5de353bb883407a5575cc025aa20f48
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-08 17:20:13 +0900

    IOCP 서버 복습 중간커밋

--- IOCP/IOCP_SERVER/main.cpp
+++ IOCP/IOCP_SERVER/main.cpp
@@ -1,28 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <process.h>
 #include <string>
 #include <WinSock2.h>
+#include <windows.h>
 
-const static int BUF_SIZE = 1024;
-void CALLBACK ReadCompRoutine(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
-void CALLBACK WriteCompRoutine(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+const static int BUF_SIZE = 100;
+const static int READ = 3;
+const static int WRITE = 5;
+
+unsigned int WINAPI EchoThreadMain(LPVOID CompletionPortIO);
 void ErrorHandling(const char* msg);
 
 typedef struct
 {
 	SOCKET hClntSock;
-	char buf[BUF_SIZE];
+	SOCKADDR_IN clntAdr;
+} PER_HANDLE_DATA, * LPPER_HANDLE_DATA;
+
+typedef struct
+{
+	OVERLAPPED overlapped;
 	WSABUF wsaBuf;
-} PER_IO_DATA, *LPPER_IO_DATA;
+	char buffer[BUF_SIZE];
+	int rwMode;
+} PER_IO_DATA, * LPPER_IO_DATA;
 
 int main(int argc, const char* argv[])
 {
 	WSADATA wsaData;
-	SOCKET hLisnSock, hRecvSock;
-	SOCKADDR_IN lisnAdr, recvAdr;
-	LPWSAOVERLAPPED lpOvlp;
-	DWORD recvBytes;
-	LPPER_IO_DATA hbInfo;
-	int mode = 1, recvAdrSz = 0, flagInfo = 0;
+	HANDLE hComPort;
+	SYSTEM_INFO sysInfo;
+	LPPER_IO_DATA ioInfo;
+	LPPER_HANDLE_DATA handleInfo;
+
+	SOCKET hServSock;
+	SOCKADDR_IN servAdr;
+	int recvBytes = 0, i = 0, flags = 0;
 
 	if (argc != 2)
 	{
@@ -35,81 +49,91 @@ int main(int argc, const char* argv[])
 		ErrorHandling("WSAStartup() error");
 	}
 
-	hLisnSock = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-	ioctlsocket(hLisnSock, FIONBIO, (u_long*)&mode);
-	memset(&lisnAdr, 0, sizeof(lisnAdr));
-	lisnAdr.sin_family = AF_INET;
-	lisnAdr.sin_addr.s_addr = htonl(INADDR_ANY);
-	lisnAdr.sin_port = htons(atoi(argv[1]));
+	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	GetSystemInfo(&sysInfo);
+	for (i = 0; i < sysInfo.dwNumberOfProcessors; i++)
+		_beginthreadex(NULL, 0, EchoThreadMain, (LPVOID)hComPort, 0, NULL);
 
-	if (bind(hLisnSock, (SOCKADDR*)&lisnAdr, sizeof(lisnAdr)) == SOCKET_ERROR)
+	hServSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
 		ErrorHandling("bind() error");
-	if (listen(hLisnSock, 5) == SOCKET_ERROR)
+	if (listen(hServSock, 5) == SOCKET_ERROR)
 		ErrorHandling("listen() error");
-
-	recvAdrSz = sizeof(recvAdr);
 
 	while (1)
 	{
-		SleepEx(100,TRUE);
-		hRecvSock = accept(hLisnSock, (SOCKADDR*)&recvAdr, &recvAdrSz);
-		if (hRecvSock == INVALID_SOCKET)
-		{
-			if (WSAGetLastError() == WSAEWOULDBLOCK)
-				continue;
-			else
-			{
-				ErrorHandling("accept() error");
-			}
-		}
-		puts("Client connected..");
-		lpOvlp = new WSAOVERLAPPED;
-		memset(lpOvlp, 0, sizeof(WSAOVERLAPPED));
+		SOCKET hClntSock;
+		SOCKADDR_IN clntAdr;
+		int addrLen = sizeof(clntAdr);
+		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
+		handleInfo = new PER_HANDLE_DATA();
+		handleInfo->hClntSock = hClntSock;
+		memcpy(&(handleInfo->clntAdr), &clntAdr, addrLen);
 
-		hbInfo = new PER_IO_DATA;
-		hbInfo->hClntSock = (DWORD)hRecvSock;
-		(hbInfo->wsaBuf).buf = hbInfo->buf;
-		(hbInfo->wsaBuf).len = BUF_SIZE;
+		CreateIoCompletionPort((HANDLE)hClntSock, hComPort, (DWORD)handleInfo, 0);
 
-		lpOvlp->hEvent = (HANDLE)hbInfo;
-		WSARecv(hRecvSock, &(hbInfo->wsaBuf), 1, &recvBytes, (LPDWORD)&flagInfo, lpOvlp, ReadCompRoutine);
+		ioInfo = new PER_IO_DATA();
+		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+		ioInfo->wsaBuf.buf = ioInfo->buffer;
+		ioInfo->wsaBuf.len = BUF_SIZE;
+		ioInfo->rwMode = READ;
+		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf), 1, (LPDWORD)&recvBytes, (LPDWORD)&flags, &(ioInfo->overlapped), NULL);
 	}
 
-	closesocket(hLisnSock);
-	closesocket(hRecvSock);
-	WSACleanup();
 	return 0;
 }
 
-void CALLBACK ReadCompRoutine(DWORD dwError, DWORD szRecvBytes, LPWSAOVERLAPPED lpOverlapped, DWORD flags)
+unsigned int WINAPI EchoThreadMain(LPVOID pComPort)
 {
-	LPPER_IO_DATA hbInfo = (LPPER_IO_DATA)(lpOverlapped->hEvent);
-	SOCKET hSock = hbInfo->hClntSock;
-	LPWSABUF bufInfo = &(hbInfo->wsaBuf);
-	DWORD sentBytes;
-	if (szRecvBytes == 0)
+	HANDLE hComPort = (HANDLE)pComPort;
+	SOCKET sock;
+	DWORD bytesTrans;
+	LPPER_HANDLE_DATA handleInfo;
+	LPPER_IO_DATA ioInfo;
+	DWORD flags = 0;
+
+	while (1)
 	{
-		closesocket(hSock);
-		delete(lpOverlapped->hEvent);
-		delete(lpOverlapped);
-		puts("Client disconnected..");
+		GetQueuedCompletionStatus(hComPort, &bytesTrans, (PULONG_PTR)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
+		sock = handleInfo->hClntSock;
+
+		if (ioInfo->rwMode == READ)
+		{
+			puts("message received!");
+			if (bytesTrans == 0)
+			{
+				closesocket(sock);
+				delete handleInfo;
+				delete ioInfo;
+				continue;
+			}
+
+			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+			ioInfo->wsaBuf.len = bytesTrans;
+			ioInfo->rwMode = WRITE;
+			WSASend(sock, &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+
+			ioInfo = new PER_IO_DATA;
+			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+			ioInfo->wsaBuf.buf = ioInfo->buffer;
+			ioInfo->wsaBuf.len = BUF_SIZE;
+			ioInfo->rwMode = READ;
+			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags, &(ioInfo->overlapped), NULL);
+		}
+		else
+		{
+			puts("message sent!");
+			delete ioInfo;
+		}
 	}
-	else
-	{
-		bufInfo->len = szRecvBytes;
-		WSASend(hSock, bufInfo, 1, &sentBytes, 0, lpOverlapped, WriteCompRoutine);
-	}
+	return 0;
 }
 
-void CALLBACK WriteCompRoutine(DWORD dwError, DWORD szSendBytes, LPWSAOVERLAPPED lpOverlapped, DWORD flags)
-{
-	LPPER_IO_DATA hbInfo = (LPPER_IO_DATA)(lpOverlapped->hEvent);
-	SOCKET hSock = hbInfo->hClntSock;
-	LPWSABUF bufInfo = &(hbInfo->wsaBuf);
-	DWORD recvBytes;
-	int flagInfo = 0;
-	WSARecv(hSock, bufInfo, 1, &recvBytes, (LPDWORD)&flagInfo, lpOverlapped, ReadCompRoutine);
-}
 
 void ErrorHandling(const char* msg)
 {
