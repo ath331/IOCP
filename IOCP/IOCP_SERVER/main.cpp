abe0dc7552fd9aaba857b210a9fbd9562a45d798
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-10 18:13:05 +0900

    [리펙토링]주석제거

--- IOCP/IOCP_SERVER/main.cpp
+++ IOCP/IOCP_SERVER/main.cpp
@@ -1,135 +1,18 @@
-//#include <iostream>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <process.h>
-//#include <string>
-//#include <WinSock2.h>
-//#include <windows.h>
+#include<iostream>
 
-//const static int BUF_SIZE = 100;
+#include "Server/Server.h"
 
-//unsigned int WINAPI EchoThreadMain(LPVOID CompletionPortIO);
 void ErrorHandling(const char* msg);
-
-//typedef struct
-//{
-//	SOCKET hClntSock;
-//	SOCKADDR_IN clntAdr;
-//} PER_HANDLE_DATA, * LPPER_HANDLE_DATA;
-
-//typedef struct
-//{
-//	OVERLAPPED overlapped;
-//	WSABUF wsaBuf;
-//	char buffer[BUF_SIZE];
-//	int rwMode;
-//} PER_IO_DATA, * LPPER_IO_DATA;
 
 int main()
 {
-	//WSADATA wsaData;
-	//HANDLE hComPort;
-	//SYSTEM_INFO sysInfo;
-	//LPPER_IO_DATA ioInfo;
-	//LPPER_HANDLE_DATA handleInfo;
-
-	/*SOCKET hServSock;
-	SOCKADDR_IN servAdr;*/
-	//int recvBytes = 0, i = 0, flags = 0;
-	//int portNum = 0;
-	
-	/*std::cout << "Input PortNum : ";
-	std::cin >> portNum;*/
-
-	/*if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	{
-		ErrorHandling("WSAStartup() error");
-	}*/
-
-	//hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
-	//GetSystemInfo(&sysInfo);
-	/*for (i = 0; i < sysInfo.dwNumberOfProcessors; i++)
-		_beginthreadex(NULL, 0, EchoThreadMain, (LPVOID)hComPort, 0, NULL);*/
-
-	/*hServSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-	memset(&servAdr, 0, sizeof(servAdr));
-	servAdr.sin_family = AF_INET;
-	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAdr.sin_port = portNum;*/
-
-	/*if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
-		ErrorHandling("bind() error");
-	if (listen(hServSock, 5) == SOCKET_ERROR)
-		ErrorHandling("listen() error");*/
-
-	/*while (1)
-	{
-		SOCKET hClntSock;
-		SOCKADDR_IN clntAdr;
-		int addrLen = sizeof(clntAdr);
-		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
-		handleInfo = new PER_HANDLE_DATA();
-		handleInfo->hClntSock = hClntSock;
-		memcpy(&(handleInfo->clntAdr), &clntAdr, addrLen);
-
-		CreateIoCompletionPort((HANDLE)hClntSock, hComPort, (ULONG_PTR)handleInfo, 0);
-
-		ioInfo = new PER_IO_DATA();
-		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
-		ioInfo->wsaBuf.buf = ioInfo->buffer;
-		ioInfo->wsaBuf.len = BUF_SIZE;
-		ioInfo->rwMode = READ;
-		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf), 1, (LPDWORD)&recvBytes, (LPDWORD)&flags, &(ioInfo->overlapped), NULL);
-	}*/
+	Server server;
+	server.InputPortNum();
+	server.InitServer();
+	server.RunServer();
 
 	return 0;
 }
-
-//unsigned int WINAPI EchoThreadMain(LPVOID pComPort)
-//{
-//	HANDLE hComPort = (HANDLE)pComPort;
-//	SOCKET sock;
-//	DWORD bytesTrans;
-//	LPPER_HANDLE_DATA handleInfo;
-//	LPPER_IO_DATA ioInfo;
-//	DWORD flags = 0;
-//
-//	while (1)
-//	{
-//		GetQueuedCompletionStatus(hComPort, &bytesTrans, (PULONG_PTR)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
-//		sock = handleInfo->hClntSock;
-//
-//		if (ioInfo->rwMode == READ)
-//		{
-//			puts("message received!");
-//			if (bytesTrans == 0)
-//			{
-//				closesocket(sock);
-//				delete handleInfo;
-//				delete ioInfo;
-//				continue;
-//			}
-//
-//			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
-//			ioInfo->wsaBuf.len = bytesTrans;
-//			ioInfo->rwMode = WRITE;
-//			WSASend(sock, &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-//
-//			ioInfo = new PER_IO_DATA;
-//			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
-//			ioInfo->wsaBuf.buf = ioInfo->buffer;
-//			ioInfo->wsaBuf.len = BUF_SIZE;
-//			ioInfo->rwMode = READ;
-//			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags, &(ioInfo->overlapped), NULL);
-//		}
-//		else
-//		{
-//			puts("message sent!");
-//			delete ioInfo;
-//		}
-//	}
-//	return 0;
-//}
 
 
 void ErrorHandling(const char* msg)
