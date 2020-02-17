4f84a3c75ea32c3b272786c5c84b265f9e1ac4ff
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-17 23:27:38 +0900

    [Client][리펙토링]Dialog관련 Class화
    TODO : DialogBox함수가 자꾸 중단점을 리턴해서 Dialog를 꺼도 다른 Dialog가 켜지고 그런다. 원인을 찾는중

---
+++ Client/Interface/DialogManager/DialogManager.cpp
@@ -0,0 +1,296 @@
+#include "DialogManager.h"
+
+DialogManager* DialogManager::_instance = NULL;
+
+
+void DialogManager::MakeDialog(DialogType dialogType)
+{
+	switch (dialogType)
+	{
+	case DialogType::Login:
+		DialogBox(_hInst, MAKEINTRESOURCE(IDD_ChatLogin), NULL, (DLGPROC)DlgProcLogin);
+		break;
+	case DialogType::Main:
+		DialogBox(_hInst, MAKEINTRESOURCE(IDD_ChatMain), NULL, (DLGPROC)DlgProcMain);
+		break;
+	case DialogType::MakeRoom:
+		DialogBox(_hInst, MAKEINTRESOURCE(IDD_MAKE_ROOM), NULL, (DLGPROC)DlgProcMakeRoom);
+		break;
+	case DialogType::ChatRoom:
+		DialogBox(_hInst, MAKEINTRESOURCE(IDD_ChatROom), NULL, (DLGPROC)DlgProcChatRoom);
+		break;
+	default:
+		break;
+	}
+}
+
+BOOL CALLBACK DialogManager::DlgProcLogin(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int msgboxID = 0;
+	HBRUSH g_hbrBackground = _instance->g_hbrBackground;
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		g_hbrBackground = CreateSolidBrush(RGB(128, 128, 128));
+
+		SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	case WM_CTLCOLORDLG:
+		return (LONG)g_hbrBackground;
+	case WM_CTLCOLORSTATIC:
+	{
+		HDC hdcStatic = (HDC)wParam;
+		SetTextColor(hdcStatic, RGB(255, 255, 255));
+		SetBkMode(hdcStatic, TRANSPARENT);
+		return (LONG)g_hbrBackground;
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_ENTER:
+			if (_instance->_clientLogic->Connect() == 0)
+			{
+				TCHAR name[20];
+				GetDlgItemText(hwnd, ID_NAME, name, 10);
+				std::string tempName;
+				tempName = name;
+				_instance->_clientLogic->SetName(tempName);
+				PacketLogin* packetLogin = new PacketLogin();
+				memcpy((void*)packetLogin->name, name, sizeof(name));
+				_instance->_clientLogic->SendPacket(PacketIndex::Login, (const char*)packetLogin);
+				//delete packetLogin;
+				EndDialog(hwnd, 0);
+				_instance->MakeDialog(DialogType::Main);
+			}
+			else
+			{
+				msgboxID = MessageBox(hwnd, "서버와 연결실패", "연결실패", MB_OK);
+				if (msgboxID == 6)
+					EndDialog(hwnd, 0);
+			}
+			break;
+		case ID_EXIT:
+			msgboxID = MessageBox(hwnd, "프로그램을 종료할까요?", "종료확인", MB_YESNO);
+
+			if (msgboxID == 6)
+				EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		DeleteObject(g_hbrBackground);
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
+BOOL CALLBACK DialogManager::DlgProcMain(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int msgboxID = 0;
+	HBRUSH g_hbrBackground = _instance->g_hbrBackground;
+
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		g_hbrBackground = CreateSolidBrush(RGB(128, 128, 128));
+
+		SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	case WM_CTLCOLORDLG:
+		return (LONG)g_hbrBackground;
+	case WM_CTLCOLORSTATIC:
+	{
+		HDC hdcStatic = (HDC)wParam;
+		SetTextColor(hdcStatic, RGB(255, 255, 255));
+		SetBkMode(hdcStatic, TRANSPARENT);
+		return (LONG)g_hbrBackground;
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_MAKE_ROOM:
+		{
+			_instance->MakeDialog(DialogType::MakeRoom);
+		}
+		break;
+
+		case ID_ENTER:
+		{
+			//TODO : 방목록체크 데이터 가져와서 접속 패킷전송 구현
+
+		}
+		break;
+		case ID_EXIT:
+			msgboxID = MessageBox(hwnd, "프로그램을 종료할까요?", "종료확인", MB_YESNO);
+
+			if (msgboxID == 6) //확인버튼 누름
+				EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		DeleteObject(g_hbrBackground);
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
+BOOL CALLBACK DialogManager::DlgProcMakeRoom(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HBRUSH g_hbrBackground = _instance->g_hbrBackground;
+
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		g_hbrBackground = CreateSolidBrush(RGB(128, 128, 128));
+
+		SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	case WM_CTLCOLORDLG:
+		return (LONG)g_hbrBackground;
+	case WM_CTLCOLORSTATIC:
+	{
+		HDC hdcStatic = (HDC)wParam;
+		SetTextColor(hdcStatic, RGB(255, 255, 255));
+		SetBkMode(hdcStatic, TRANSPARENT);
+		return (LONG)g_hbrBackground;
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_ROOM_PRIVATE:
+		{
+			//TODO : 비밀번호 관련 만들기
+			/*if (SendMessage((HWND)IDC_ROOM_PRIVATE, BM_GETCHECK, 0, 0) == BST_UNCHECKED)
+			{
+				SendMessage((HWND)IDC_ROOM_PRIVATE, BM_SETCHECK, BST_CHECKED, 0);
+				SendMessage((HWND)IDC_ROOM_PASSWORD, EM_SETREADONLY, false, 0);
+				cout << "BST_CHECKED" << endl;
+			}
+			else if (SendMessage((HWND)IDC_ROOM_PRIVATE, BM_GETCHECK, 0, 0) == BST_CHECKED)
+			{
+				SendMessage((HWND)IDC_ROOM_PRIVATE, BM_SETCHECK, BST_UNCHECKED, 0);
+				SendMessage((HWND)IDC_ROOM_PASSWORD, EM_SETREADONLY, true, 0);
+				cout << "BST_UNCHECKED" << endl;
+			}*/
+		}
+		break;
+
+		case IDOK:
+		{
+			PacketMakeRoom packetMakeRoom;
+			GetDlgItemText(hwnd, IDC_ROOM_NAME, packetMakeRoom.roomName, 10);
+			packetMakeRoom.maxClientCount = GetDlgItemInt(hwnd, IDC_ROOM_MAX_CLIENT_NUM, NULL, FALSE);
+			if (packetMakeRoom.maxClientCount < 2)
+				packetMakeRoom.maxClientCount = 2;
+			else if (packetMakeRoom.maxClientCount > 10)
+				packetMakeRoom.maxClientCount = 10;
+
+			PacketIndex packetIndex = PacketIndex::MAKE_ROOM;
+			int temp = _instance->_clientLogic->SendPacket(packetIndex, (const char*)&packetMakeRoom);
+			if (temp != -1 && packetIndex == PacketIndex::MAKE_ROOM)
+			{
+				//temp = roomNum
+				_instance->_roomName = to_string(temp);
+				_instance->_roomName += "번방 ";
+				_instance->_roomName += packetMakeRoom.roomName;
+				EndDialog(hwnd, 0);
+				_instance->MakeDialog(DialogType::ChatRoom);
+			}
+		}
+		break;
+
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		DeleteObject(g_hbrBackground);
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
+BOOL CALLBACK DialogManager::DlgProcChatRoom(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HBRUSH g_hbrBackground = _instance->g_hbrBackground;
+
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		g_hbrBackground = CreateSolidBrush(RGB(128, 128, 128));
+
+		SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)LoadIcon(NULL,
+			MAKEINTRESOURCE(IDI_APPLICATION)));
+		SetWindowText(hwnd, _instance->_roomName.c_str());
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	case WM_CTLCOLORDLG:
+		return (LONG)g_hbrBackground;
+	case WM_CTLCOLORSTATIC:
+	{
+		HDC hdcStatic = (HDC)wParam;
+		SetTextColor(hdcStatic, RGB(255, 255, 255));
+		SetBkMode(hdcStatic, TRANSPARENT);
+		return (LONG)g_hbrBackground;
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_ROOM_PRIVATE:
+		{
+		}
+		break;
+
+		case IDOK:
+		{
+		}
+		break;
+
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		DeleteObject(g_hbrBackground);
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
---
+++ Client/Interface/DialogManager/DialogManager.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "ClientLogic.h"
+#include <windows.h>
+#include <string>
+
+#include "../../Packet/packet.h"
+#include "../resource.h"
+
+using namespace std;
+
+enum class DialogType : int
+{
+	Login,
+	Main,
+	MakeRoom,
+	ChatRoom,
+};
+
+class DialogManager
+{
+public:
+	DialogManager(HINSTANCE hInst, ClientLogic* clientLogic): _hInst(hInst), _clientLogic(clientLogic)
+	{
+		_instance = this;
+	}
+
+	void MakeDialog(DialogType dialogType);
+
+private:
+	static DialogManager* _instance;
+	ClientLogic* _clientLogic;
+	string _roomName;
+
+	HINSTANCE _hInst;
+	HBRUSH g_hbrBackground;
+
+	static BOOL CALLBACK DlgProcLogin(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+	static BOOL CALLBACK DlgProcMain(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+	static BOOL CALLBACK DlgProcMakeRoom(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+	static BOOL CALLBACK DlgProcChatRoom(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+};
