6234d063b458bea392d4badd9d58ab294ba6119b
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-26 22:36:17 +0900

    ClientRoomNum 관리 구조생각중
    Client가 방을 만드는 알고리즘을
    방만들기 -> 방접속 으로 분리해서
    방접속 알고리즘에서 클라이언트가 방금 들어간 방의 번호를 가지게 만들기
    (방만들기와 방접속 분리, Client가 들어가 있는 RoomNum관리 용이)

--- IOCP/IOCP_SERVER/Basic/ClientInfo.h
+++ IOCP/IOCP_SERVER/Basic/ClientInfo.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <WinSock2.h>
-
+#include <vector>
 const static int MAX_CLIENT_NAME_LENGTH = 10;
+
+using namespace std;
 
 struct ClientSocketInfo
 {
@@ -13,11 +15,10 @@ struct ClientInfo
 {
 	SOCKET clientSock;
 	const char clientName[MAX_CLIENT_NAME_LENGTH] = "DEFAULT";
-	int roomNum = -1;
+	vector<int> rooomNum;
 
 	ClientInfo& operator=(ClientInfo clientInfo)
 	{
-		roomNum = clientInfo.roomNum;
 		clientSock = clientInfo.clientSock;
 		memcpy((void*)clientName, clientInfo.clientName,sizeof(clientInfo.clientName));
 		return *this;
