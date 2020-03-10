2a4f5eb7a43e97654a02dab656af7e26142055e2
ath331 <39950003+ath331@users.noreply.github.com>
2020-03-10 17:20:18 +0900

    [리펙토링[ClientInfo 수정

---
+++ IOCP/IOCP_SERVER/Basic/ClientInfo.cpp
@@ -0,0 +1,26 @@
+#include "ClientInfo.h"
+
+ClientInfo::ClientInfo(SOCKET sock)
+{
+	clientSock = sock;
+}
+
+void ClientInfo::OutRoom(int _roomNum)
+{
+	roomNum = -1;
+}
+
+bool ClientInfo::IsAcceptingRoomNum(int _roomNum)
+{
+	if (_roomNum != -1)
+		return TRUE;
+	return FALSE;
+}
+
+const ClientInfo& ClientInfo::operator=(ClientInfo clientInfo)
+{
+	clientSock = clientInfo.clientSock;
+	memcpy((void*)clientName, clientInfo.clientName, sizeof(clientInfo.clientName));
+	roomNum = clientInfo.roomNum;
+	return *this;
+}
--- IOCP/IOCP_SERVER/Basic/ClientInfo.h
+++ IOCP/IOCP_SERVER/Basic/ClientInfo.h
@@ -1,12 +1,7 @@
 #pragma once
-#include <iostream>
 #include <WinSock2.h>
-#include <vector>
-#include <algorithm>
 
 const static int MAX_CLIENT_NAME_LENGTH = 10;
-
-using namespace std;
 
 struct ClientSocketInfo
 {
@@ -18,43 +13,11 @@ struct ClientInfo
 {
 	SOCKET clientSock;
 	const char clientName[MAX_CLIENT_NAME_LENGTH] = "DEFAULT";
-	//vector<int> roomNum;
 	int roomNum = -1;
-	ClientInfo(SOCKET sock)
-	{
-		clientSock = sock;
-	}
-	void OutRoom(int _roomNum)
-	{
-		//roomNum.erase(remove(roomNum.begin(), roomNum.end(), _roomNum), roomNum.end());
-		roomNum = -1;
-	}
+	ClientInfo(SOCKET sock);
+	void OutRoom(int _roomNum);
 
-	bool IsAcceptingRoomNum(int _roomNum)
-	{
-		/*int i = 0;
-		for (auto iter = roomNum.begin(); iter != roomNum.end(); iter++)
-		{
-			if (roomNum[i] == _roomNum)
-			{
-				return TRUE;
-			}
-			i++;
-		}*/
-		if (_roomNum != -1)
-			return TRUE;
-		return FALSE;
-	}
+	bool IsAcceptingRoomNum(int _roomNum);
 
-	ClientInfo& operator=(ClientInfo clientInfo)
-	{
-		clientSock = clientInfo.clientSock;
-		memcpy((void*)clientName, clientInfo.clientName, sizeof(clientInfo.clientName));
-		roomNum = clientInfo.roomNum;
-		/*for (int i = 0; i < clientInfo.roomNum.size(); i++)
-		{
-			roomNum[i] = clientInfo.roomNum[i];
-		}*/
-		return *this;
-	}
+	const ClientInfo& operator=(ClientInfo clientInfo);
 };
