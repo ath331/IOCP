26217fa46a7bc105b5d4bcf5e8cb8d2546e78fa1
ath331 <39950003+ath331@users.noreply.github.com>
2020-04-17 20:23:07 +0900

    [Client]DummyClient 프로젝트 추가
    서버성능 테스트를위해 DummyClient를 생성하는 프로젝트 추가

---
+++ Client/DummyClient/main.cpp
@@ -0,0 +1,13 @@
+/*
+Dummy Client를 만들어서 서버성능을 테스트해보기위한 프로젝트.
+실제 사용자들의 활동을 따라 할 수있다. (로그인, 방만들기, 방접속, 채팅등등)
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Dummy Client Project" << endl;
+}
