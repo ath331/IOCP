0ce1a8f75e2854b23b0ced72b38972d7338b9ba3
ath331 <39950003+ath331@users.noreply.github.com>
2020-04-17 17:41:58 +0900

    Error 관련 class 추가

--- IOCP/IOCP_SERVER/Basic/Log/Log.h
+++ IOCP/IOCP_SERVER/Basic/Log/Log.h
@@ -1,7 +1,34 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+enum class LogIndex : int
+{
+	WARNING,
+	ERROR,
+};
+
 class Log
 {
 public:
+	Log(LogIndex index, const char* errStr)
+	{
+		switch (index)
+		{
+		case LogIndex::WARNING:
+			cout << "Warning : " << errStr << endl;
+			break;
+
+		case LogIndex::ERROR:
+			cout << "Error : " << errStr << endl;
+			exit(1);
+			break;
+
+		default:
+			break;
+		}
+	}
 private:
 };
 
