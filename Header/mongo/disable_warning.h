bbf386496402b33c48e549f0b93c8ee67ad96117
ath331 <39950003+ath331@users.noreply.github.com>
2020-06-24 16:40:24 +0900

    add MongoDB Header

---
+++ Header/mongo/disable_warning.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma warning(disable:4141)
+#pragma warning(disable:4244)
+#pragma warning(disable:4267)
+#pragma warning(disable:4800)
---
+++ Header/mongo/win32_lib.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#pragma comment(lib, "mongodbclient.lib")
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "dbghelp.lib")
