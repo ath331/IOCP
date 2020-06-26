cb3fee7a11558356edb5e9e70f560c6381ca45bc
ath331 <39950003+ath331@users.noreply.github.com>
2020-06-26 16:40:15 +0900

    NoSQL Class 추가
    NoSQL을 위한 정적라이브러리 프로젝트 및 Class 생성

---
+++ IOCP/NoSQL/NoSQL.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../../Header/MongoDB/mongo.h"
+#include "../../Header/MongoDB/bson.h"
+
+const char* SERVER = "mongodb://localhost:27017";
+const char* DATABASE = "Test01";
+
+class NoSQL
+{
+public:
+
+private:
+};
