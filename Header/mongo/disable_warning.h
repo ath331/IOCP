d7423d995e6fe2a9508af9d69622985759c63115
ath331 <39950003+ath331@users.noreply.github.com>
2020-06-26 11:05:22 +0900

    del mongoDB header

--- Header/mongo/disable_warning.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define _CRT_SECURE_NO_WARNINGS
-
-#pragma warning(disable:4141)
-#pragma warning(disable:4244)
-#pragma warning(disable:4267)
-#pragma warning(disable:4800)
--- Header/mongo/win32_lib.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#pragma comment(lib, "mongodbclient.lib")
-#pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "dbghelp.lib")
